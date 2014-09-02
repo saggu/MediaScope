cc099577dfb64f1dd00c23ebac6b187e8a322d5a
saggu <amandeep.s.saggu@gmail.com>
2014-09-01 17:17:05 -0700

    Process exif metadata and publish to triplestore

--- media_scope_server/publish_rdf.py
+++ media_scope_server/publish_rdf.py
@@ -24,16 +24,19 @@ CONTENT_TYPE_JSON = "JSON"
 CONTENT_TYPE_XML = "XML"
 REFRESH_MODEL = "RefreshModel"
 
-#Form parameters value - SHOULD BE READ FROM A CONFIG FILE LATER
-SPARQL_ENDPOINT_VALUE = "http://fusion-sqid.isi.edu:8890/sparql-graph-crud-auth/"
-GRAPH_URI_VALUE = "http://fusion-sqid.isi.edu:8890/image-metadata" #could be any URI
-R2RML_URL_VALUE = "http://fusion-sqid.isi.edu/WSP1WS2-metadata.json-model.ttl"
-DATA_URL_VALUE = "DataURL"
-USERNAME_VALUE = "finimg"
-PASSWORD_VALUE = "isi"
-OVERWRITE_VALUE = "True"
-REFRESH_MODEL_VALUE = "True"
-RAW_DATA_VALUE = "{\"metadata\":{\"GPSTimeStamp\":\"NOT_AVAILABLE\",\"ISOSpeedRatings\":\"100\",\"Orientation\":\"6\",\"Model\":\"GT-N7100\",\"WhiteBalance\":\"0\",\"GPSLongitude\":\"NOT_AVAILABLE\",\"ImageLength\":\"2448\",\"FocalLength\":\"3.7\",\"HasFaces\":\"1\",\"ImageName\":\"20140707_134558.jpg\",\"GPSDateStamp\":\"NOT_AVAILABLE\",\"Flash\":\"0\",\"DateTime\":\"2014:07:07 13:45:58\",\"NumberOfFaces\":\"1\",\"ExposureTime\":\"0.020\",\"GPSProcessingMethod\":\"NOT_AVAILABLE\",\"FNumber\":\"2.6\",\"ImageWidth\":\"3264\",\"GPSLatitude\":\"NOT_AVAILABLE\",\"GPSAltitudeRef\":\"-1\",\"Make\":\"SAMSUNG\",\"GPSAltitude\":\"-1.0\"}}"
+#Triple Store URl
+SPARQL_ENDPOINT_VALUE = "http:"
+#could be any URI
+GRAPH_URI_VALUE = "http://" 
+#URL to fetch the R2RML model
+R2RML_URL_VALUE = "http://"
+DATA_URL_VALUE = "http://"
+USERNAME_VALUE = "<username>"
+PASSWORD_VALUE = "<password>"
+#Overwrite the existing data True or False
+OVERWRITE_VALUE = 
+#Fetch the updated model from the URL <True or False>
+REFRESH_MODEL_VALUE = 
 
 
 
@@ -52,10 +55,10 @@ def publish_rdf():
 def call_webservice(rawdata):
         params = urllib.urlencode({SPARQL_ENDPOINT : SPARQL_ENDPOINT_VALUE, GRAPH_URI : GRAPH_URI_VALUE,
                                    TRIPLE_STORE : TRIPLE_STORE_VIRTUOSO, OVERWRITE : OVERWRITE_VALUE , REFRESH_MODEL : REFRESH_MODEL_VALUE,
-                                   CONTENT_TYPE : CONTENT_TYPE_JSON, RAW_DATA : RAW_DATA_VALUE, R2RML_URL : R2RML_URL_VALUE, USERNAME : USERNAME_VALUE, PASSWORD : PASSWORD_VALUE})
+                                   CONTENT_TYPE : CONTENT_TYPE_JSON, DATA_URL : DATA_URL_VALUE, R2RML_URL : R2RML_URL_VALUE, USERNAME : USERNAME_VALUE, PASSWORD : PASSWORD_VALUE})
         headers = {"Content-type": "application/x-www-form-urlencoded"}
-        conn = httplib.HTTPConnection("fusion-sqid.isi.edu","8080")
-        conn.request("POST", "/publishrdf/rdf/r2rml/rdf/sparql", params, headers)
+        conn = httplib.HTTPConnection("fusion-sqid.isi.edu","8080") # URL and PORT for Triplestore
+        conn.request("POST", "/publishrdf/rdf/r2rml/rdf/sparql", params, headers) #the path for the REST web service
         response = conn.getresponse()
         print response.status, response.reason
         data = response.read()
